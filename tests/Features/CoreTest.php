8892543d96e231377a3818d91cd05ceb47e5a522
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-05-15 12:19:44 +0800

    Setup Unit Test

---
+++ tests/Features/CoreTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CleaniqueCoders\NadiLaravel\Tests\Features;
+
+use CleaniqueCoders\NadiLaravel\Tests\TestCase;
+
+class CoreTest extends TestCase
+{
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // $this->artisan('nadi:install');
+    }
+
+    /**
+     * A basic test example.
+     */
+    public function test_core(): void
+    {
+        $this->assertTrue(true);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,7 @@
 
 namespace CleaniqueCoders\NadiLaravel\Tests;
 
-use PHPUnit\Framework\TestCase as BaseTestCase;
-
-abstract class TestCase extends BaseTestCase
+abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
     //
 }
